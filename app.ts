a885405fc61c3453023222120e281681722ba2c5
Pau Garcia Valero <pau.garcia.valero@gmail.com>
2024-10-04 22:12:56 +0200

    chore(): adds static html page

--- app.ts
+++ app.ts
@@ -1,12 +1,12 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 
 app.use("*", logger());
 
-app.get("/", (c) => {
-  return c.text("up");
-});
+app.use("/static/*", serveStatic({ root: "./public" }));
+app.get("/", serveStatic({ path: "./public/index.html" }));
 
 export default app;
